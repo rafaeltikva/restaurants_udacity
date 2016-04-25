04056b7b49e9751012db7fedb7f7680b65bbdd30
Rafael Tikva <rafaeltikva@Rafaels-MacBook-Pro.local>
2016-04-26 01:14:33 +0300

    add the correct restaurants_flask version
    - delete restaurants.py (the basehttpserver version)

--- database.py
+++ database.py
@@ -14,48 +14,82 @@ def create_session():
     return DBSession()
 
 
-def query(object_type, filter=''):
+def query_all(object_type, filter=''):
     # initialize query_results
-    query_results = ''
+
+    response = ''
     try:
 
-        object_type = getattr(database_setup, object_type)
+        object_type = getattr(database_setup, object_type) if type(object_type) == str else object_type
 
     except AttributeError as e:
         print e
         return e
 
-    session = create_session()
-
     try:
-        query_results = session.query(object_type).all()
+        session = create_session()
+        response = session.query(object_type).all()
 
     except NoResultFound:
-        query_results = 'No results were found'
+        response = 'No results were found'
 
-    session.close()
+    finally:
+        session.close()
 
-    print query_results
-    print type(query_results)
-    return query_results
+    print response
+    print type(response)
+    return response
+
+
+def query(object_type, id, to_dict = False):
+    # initialize response
+    response = {}
+
+    try:
+
+        object_type = getattr(database_setup, object_type) if type(object_type) == str else object_type
+
+    except AttributeError as e:
+        print e
+        return e
+
+    try:
+        session = create_session()
+
+        # update found store
+
+        if to_dict:
+            response['object'] = session.query(object_type).filter_by(id=id).one().to_dict()
+        else:
+            response = session.query(object_type).filter_by(id=id).one()
+
+    except NoResultFound:
+        response['message'] = "Object doesn't exist"
+
+    finally:
+
+        session.close()
+
+    print response
+    return response
 
 
 def create(object_type, store):
-
     # initalize response
     response = {}
 
     try:
 
-        object_type = getattr(database_setup, object_type)
+        object_type = getattr(database_setup, object_type) if type(object_type) == str else object_type
 
     except AttributeError as e:
         print e
         return e
 
-    session = create_session()
-
     try:
+
+        session = create_session()
+
         # create new object
         object_to_create = object_type(**store)
 
@@ -78,68 +112,32 @@ def create(object_type, store):
 
     except Exception as e:
         response['message'] = e.message
-
-    session.close()
-
-    return response
-
-def delete(object_type, store):
-
-    # initalize response
-    response = {}
-
-    # initialize query_result
-    query_result = ''
-
-    try:
-
-        object_type = getattr(database_setup, object_type)
-
-    except AttributeError as e:
-        print e
-        return e
-
-    session = create_session()
-
-    try:
-        object_to_delete = session.query(object_type).filter_by(id = store['id']).one()
-        session.delete(object_to_delete)
-        session.commit()
-        response['message'] = 'Success'
-        # return updated object
-        response['object'] = store
-
-    except NoResultFound:
-        response['message'] = "Object doesn't exist"
-
-    session.close()
-
-    print query_result
+    finally:
+        session.close()
 
     return response
 
 
 def update(object_type, store):
-
     # initialize response
     response = {}
 
     try:
 
-        object_type = getattr(database_setup, object_type)
+        object_type = getattr(database_setup, object_type) if type(object_type) == str else object_type
 
     except AttributeError as e:
         print e
         return e
 
-    session = create_session()
-
     try:
 
-        #object_to_update = session.query(object_type).filter_by(id = store['id']).one()
+        session = create_session()
+
+        # object_to_update = session.query(object_type).filter_by(id = store['id']).one()
 
         # update found store
-        session.query(object_type).filter_by(id = store['id']).update(store)
+        session.query(object_type).filter_by(id=store['id']).update(store)
 
         # flush session transaction
         session.commit()
@@ -151,6 +149,50 @@ def update(object_type, store):
     except NoResultFound:
         response['message'] = "Object doesn't exist"
 
-    session.close()
+    finally:
+        session.close()
+
+    return response
+
+
+def delete(object_type, store=None, id=0):
+    try:
+        # initialize id for later db query
+        id = store.get('id') if isinstance(store, dict) else id
+    except KeyError as e:
+        print e.message
+        raise e
+
+
+    # initalize response
+    response = {}
+
+    if id > 0:
+
+        try:
+
+            object_type = getattr(database_setup, object_type) if type(object_type) == str else object_type
+
+        except AttributeError as e:
+            print e
+            return e
+
+        try:
+            session = create_session()
+
+            object_to_delete = session.query(object_type).filter_by(id=id).one()
+            session.delete(object_to_delete)
+            session.commit()
+            response['message'] = 'Success'
+            # return updated object
+            response['object'] = object_to_delete.to_dict()
+
+        except NoResultFound:
+            response['message'] = "Object doesn't exist"
+
+        finally:
+            session.close()
+    else:
+        response['message'] = "Object id missing"
 
     return response
